422ed3057fc573ddfb420aa2a7b0ef5079073338
Isaac Meyer <isaacmeyer0@gmail.com>
2025-03-04 15:45:47 -0800

    finally bouncing, added translation to the vertices and made projection function generic

--- src/main.rs
+++ src/main.rs
@@ -101,11 +101,12 @@ fn kinetic_physics(
     }
 
     // Advance velocity
-    for phys_entity in &game.entities {
-        if let Ok(mut transform) = transforms.get_mut(phys_entity.entity()) {
-            let delta_pos = phys_entity.velocity() * time.delta_secs();
+    for i in 0..game.entities.len() {
+        if let Ok(mut transform) = transforms.get_mut(game.entities[i].entity()) {
+            let delta_pos = game.entities[i].velocity() * time.delta_secs();
             transform.translation.x += delta_pos.x;
             transform.translation.y += delta_pos.y;
+            game.entities[i].physics_mut().position += delta_pos;
         }
     }
 }
@@ -213,26 +214,26 @@ impl PhysicsEntity {
         }
     }
 
-    fn project(&self, axis: &Vec2) -> (f32, f32) {
-        let mut min = -f32::INFINITY;
-        let mut max = f32::INFINITY;
-        let verts = self.vertices();
-        for i in 0..verts.len() {
-            let proj = axis.dot(verts[i]);
-            if proj < min { min = proj; }
-            if proj > max { max = proj; }
-        }
-        (min, max)
+    fn translated_verts(&self) -> Vec<Vec2> {
+        self.vertices().iter().map(|v| {
+            Vec2 {
+                x: v.x + self.position().x,
+                y: v.y + self.position().y,
+            }
+        }).collect()
     }
 
     fn test_collision(&self, other: &PhysicsEntity) -> Option<Vec2> {
         let axes1 = self.get_axes();
         let axes2 = other.get_axes();
+        let verts1 = self.translated_verts();
+        let verts2 = other.translated_verts();
+
         let mut overlap = f32::INFINITY;
         let mut min_transl_vec = Vec2::ZERO;
         for axis in axes1.iter() {
-            let (min1, max1) = self.project(axis);
-            let (min2, max2) = other.project(axis);
+            let (min1, max1) = project(&verts1, axis);
+            let (min2, max2) = project(&verts2, axis);
             if max1 < min2 || max2 < min1 {
                 return None;
             }
@@ -245,8 +246,8 @@ impl PhysicsEntity {
             }
         }
         for axis in axes2.iter() {
-            let (min1, max1) = self.project(axis);
-            let (min2, max2) = other.project(axis);
+            let (min1, max1) = project(&verts1, axis);
+            let (min2, max2) = project(&verts2, axis);
             if max1 < min2 || max2 < min1 {
                 return None;
             }
@@ -261,6 +262,18 @@ impl PhysicsEntity {
         Some(min_transl_vec)
     }
 }
+
+fn project(verts: &Vec<Vec2>, axis: &Vec2) -> (f32, f32) {
+    let mut min = f32::INFINITY;
+    let mut max = -f32::INFINITY;
+    for i in 0..verts.len() {
+        let proj = axis.dot(verts[i]);
+        if proj < min { min = proj; }
+        if proj > max { max = proj; }
+    }
+    (min, max)
+}
+
 
 
 
@@ -299,12 +312,12 @@ impl RectangleEntity {
     }
 
     fn set_vertices(&mut self) {
-        let mut verts = &mut self.physics.vertices;
+        let verts = &mut self.physics.vertices;
         let pos = &self.physics.position;
-        verts.push(Vec2::new(pos.x + self.width/2.0, pos.y + self.height/2.0));
-        verts.push(Vec2::new(pos.x - self.width/2.0, pos.y + self.height/2.0));
-        verts.push(Vec2::new(pos.x - self.width/2.0, pos.y - self.height/2.0));
-        verts.push(Vec2::new(pos.x + self.width/2.0, pos.y - self.height/2.0));
+        verts.push(Vec2::new(self.width/2.0, self.height/2.0));
+        verts.push(Vec2::new(-self.width/2.0, self.height/2.0));
+        verts.push(Vec2::new(-self.width/2.0, -self.height/2.0));
+        verts.push(Vec2::new(self.width/2.0, -self.height/2.0));
     }
 }
 
@@ -343,28 +356,63 @@ mod test {
     //     assert_eq!(b.position.y, 2.0);
     // }
 
-    #[test]
-    fn distance_test() {
-        let a = Vec2 {
-            x: 0.0,
-            y: 0.0,
-        };
-        let b = Vec2 {
-            x: 1.0,
-            y: 1.0,
-        };
-        assert_eq!(distance(&a, &b), (2.0_f32).powf(0.5))
-    }
+    // #[test]
+    // fn distance_test() {
+    //     let a = Vec2 {
+    //         x: 0.0,
+    //         y: 0.0,
+    //     };
+    //     let b = Vec2 {
+    //         x: 1.0,
+    //         y: 1.0,
+    //     };
+    //     assert_eq!(distance(&a, &b), (2.0_f32).powf(0.5))
+    // }
 
     #[test]
     fn circle_vertices_test() {
-        let n = 8;
-        let circle_verts = make_circle_vertices(n, 1.0);
-        for vert in circle_verts.iter() {
+        let mut ball = BallEntity {
+            physics: PhysicsData::new(Vec2::ZERO, Vec2::ZERO),
+            bevy: BevyData::new(),
+            radius: 1.0,
+        };
+        let n: usize = 8;
+        ball.set_vertices(n as i64);
+        for vert in ball.physics.vertices.iter() {
             println!{"Circle vert: {}", vert};
         }
-        assert_eq!(circle_verts.len(), n);
+        assert_eq!(ball.physics.vertices.len(), n);
     }
 
+    // #[test]
+    // fn test_rectangle_collision() {
+    //     let rect1 = PhysicsEntity::Rectangle(RectangleEntity::new(Vec2::new(0.0, 0.0), Vec2::new(0.0, 0.0), 2.0, 2.0));
+    //     let rect2 = PhysicsEntity::Rectangle(RectangleEntity::new(Vec2::new(1.0, 0.0), Vec2::new(0.0, 0.0), 2.0, 2.0));
+    //     let rect3 = PhysicsEntity::Rectangle(RectangleEntity::new(Vec2::new(3.0, 0.0), Vec2::new(0.0, 0.0), 0.5, 0.5));
+
+    //     // Print the axes of each rectangle
+    //     for rect in vec![&rect1, &rect2, &rect3] {
+    //         println!("Rectangle axes: {:?}", rect.physics().axes);
+    //     }
+    //     
+    //     // rect1 and rect2 should collide, while rect1 and rect3 should not
+    //     println!("Testing 2x2 rectangles at position (0,0) and (1,0)\n");
+    //     assert!(rect1.test_collision(&rect2).is_some());
+    //     println!("Testing 2x2 at position (0,0) and 0.5x0.5 at position (2,0)\n");
+    //     assert!(rect1.test_collision(&rect3).is_none());
+    // }
+
+    #[test]
+    fn test_rectangle_circle_collision() {
+        let circ = PhysicsEntity::Ball(BallEntity::new(Vec2::new(1.0, 0.0), Vec2::new(0.0, 0.0), 0.5));
+        let rect1 = PhysicsEntity::Rectangle(RectangleEntity::new(Vec2::new(0.0, 0.0), Vec2::new(0.0, 0.0), 0.25, 0.25));
+        let rect2 = PhysicsEntity::Rectangle(RectangleEntity::new(Vec2::new(1.5, 0.0), Vec2::new(0.0, 0.0), 0.5, 0.5));
+
+        // rect1 and circ should not collide, while rect2 and circ should
+        println!("Testing 0.5 radius circle at position (1,1) and 0.25x0.25 rectangle at position (0,0)\n");
+        assert!(circ.test_collision(&rect1).is_none());
+        println!("Testing 0.5 radius circle at position (1,1) and 0.5x0.5 rectangle at position (1.5,0)\n");
+        assert!(circ.test_collision(&rect2).is_some());
+    }
 }
 
